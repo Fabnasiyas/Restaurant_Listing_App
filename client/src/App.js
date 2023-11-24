885249fc6a91a97166a5172f589201eea620b4fb
fabnasiyas <fabnasiyasck@gmail.com>
2023-11-24 17:11:25 +0530

    client added

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1 className='text-12xl font-bold text-blue-900 underline'>Welcome Home</h1>
+    </>
+  );
+}
+
+export default App;
